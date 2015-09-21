db7961f16411956a4545028a80eba3681d58e411
SoonHwanKim <sevuo@naver.com>
2015-09-21 20:50:12 +0900

    Update ArryTen.java

--- JAVA-algo/src/array/ArryTen.java
+++ JAVA-algo/src/array/ArryTen.java
@@ -17,8 +17,10 @@ public class ArryTen {
 	public static void main(String[] args) {
 		//int[] arr ={1,2,3,4,5};
 		int[] arr = new int[10];
+		int sum = 0;
 		for (int i = 0; i < arr.length; i++) {
 			arr [i] = i;
+			sum += arr[i];
 		}
 		System.out.println(arr);
 		}
